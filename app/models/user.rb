c6226e2d9c59f892e2138a73b4f72ea57bbe1b45
deppi0316 <d.rhymester@gmail.com>
2021-02-18 17:32:16 +0900

    devise導入→マイグレーションファイル編集→ストロングパラメータ設定→バリデーション設定

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: {with: VALID_PASSWORD_REGEX}
+  
+  validates :nickname, presence: true
+  validates :last_name_kanji, presence: true
+  validates :first_name_kanji, presence: true
+  validates :last_name_kana , presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday, presence: true
+
+
+end
